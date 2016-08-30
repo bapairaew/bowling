5c8409c545dc6bef74b7eaca3f361a7ea9e5330e
Narudom Techaval <narudom@Narudoms-MacBook.local>
2016-08-30 10:44:00 +0200

    set up project with redux

---
+++ src/containers/Game/Game.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Game.css';
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game__header">
+          <h2>Bowling Challenge</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import Game from './containers/Game/Game';
+import bowlingApp from './reducers'
 import './index.css';
 
+let store = createStore(bowlingApp)
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Game />
+  </Provider>,
   document.getElementById('root')
 );
